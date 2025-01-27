01b72ee47b6576e10647966ffe8495f3b92b6456
Emmanuel-Ejeagha <nuel6700@gmail.com>
2025-01-27 22:55:42 +0100

    mordified card component

---
+++ frontend/src/components/Card/Card.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+type Props = {};
+
+const Card = (props: Props) => {
+  return (
+    <div className="card">
+      <img src="" alt=" " />
+    </div>
+  );
+};
+
+export default Card;
